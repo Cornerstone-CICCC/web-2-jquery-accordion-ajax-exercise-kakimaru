f303da55f2212d27058dc805a5bddd443ea1c7d9
kakimaru <memimei0306@gmail.com>
2024-08-01 08:24:19 +0900

    tried ex

--- script.js
+++ script.js
@@ -1,3 +1,46 @@
-$(function() {
-  // your code here
-})
+$(function () {
+  // Exercise 1
+
+  const accordionHeader = $(".accordion-header");
+
+  accordionHeader.on("click", function () {
+    const curContent = $(this).next('.accordion-content')
+    if (curContent.is(":visible")) {
+      curContent.slideUp();
+    } else  {
+      $('.accordion-content').slideUp();
+      curContent.slideDown();
+    }
+  });
+
+  // Exercise 2
+
+  const button = $(".todos").find("button");
+
+  function getData() {
+    return new Promise((resolve, reject) => {
+      $.ajax({
+        url: `https://dummyjson.com/todos`,
+        type: `GET`,
+        success: function (response) {
+          resolve(response);
+        },
+        error: function (error) {
+          reject(error);
+        },
+      });
+    });
+  }
+
+  button.on("click", async function () {
+    const data = await getData();
+    console.log(data);
+
+    const { todos } = data;
+    console.log(todos);
+    $(this)
+      .next()
+      .after()
+      .html(todos.map((todo) => `<li>${todo.todo}</li>`));
+  });
+});
